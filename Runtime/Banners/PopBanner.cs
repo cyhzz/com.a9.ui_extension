deee9703631e405d87389dd5d0c8aa53da747a19
cyhzz <1076357483@qq.com>
2024-07-28 23:37:42 +0800

    sda

--- Runtime/Banners/PopBanner.cs
+++ Runtime/Banners/PopBanner.cs
@@ -1,4 +1,5 @@
 using Com.A9.Singleton;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 namespace Com.A9.UIExt
@@ -12,6 +13,21 @@ namespace Com.A9.UIExt
         {
             GameObject generatedPrefab = Instantiate(prefab, grid);
             Text textMeshPro = generatedPrefab.GetComponentInChildren<Text>();
+
+            if (textMeshPro != null)
+            {
+                textMeshPro.text = text;
+            }
+            else
+            {
+                Debug.LogError("TextMeshPro component not found on prefab!");
+            }
+        }
+
+        public void Text(GameObject prefab, string text, Vector2 pos)
+        {
+            GameObject generatedPrefab = Instantiate(prefab, pos, Quaternion.identity, grid);
+            TMP_Text textMeshPro = generatedPrefab.GetComponentInChildren<TMP_Text>();
 
             if (textMeshPro != null)
             {
