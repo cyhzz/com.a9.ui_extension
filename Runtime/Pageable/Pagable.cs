f56a6822da290766f475fd1751842b32eaf4d9f4
cyhzz <1076357483@qq.com>
2024-08-06 20:22:49 +0800

    af

---
+++ Runtime/Pageable/Pagable.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Com.A9.Singleton;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Pageable : MonoBehaviour
+{
+    public Button up;
+    public Button down;
+
+    public int current_page;
+    public int max_page;
+    public int CurrentPage
+    {
+        get
+        {
+            return current_page;
+        }
+        set
+        {
+            current_page = value;
+            OnPageChanged?.Invoke();
+        }
+    }
+
+    public UnityEvent OnPageChanged;
+
+    public void PageUp()
+    {
+        CurrentPage--;
+        RefreshButtonState();
+    }
+
+    public void PageDown()
+    {
+        CurrentPage++;
+        RefreshButtonState();
+    }
+
+    void RefreshButtonState()
+    {
+        up.interactable = current_page > 0;
+        down.interactable = max_page > current_page;
+    }
+}
