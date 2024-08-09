136997dcb273139e06bd359dcba1a4ae03c05ac4
cyhzz <1076357483@qq.com>
2024-08-10 03:27:44 +0800

    Update CoroutineManager.cs

--- Runtime/CurveManager/CoroutineManager/CoroutineManager.cs
+++ Runtime/CurveManager/CoroutineManager/CoroutineManager.cs
@@ -65,5 +65,32 @@ namespace Com.A9.UIExt
                 }
             }
         }
+
+        public void StartCoroutineNoInterrupt(MonoBehaviour m, IEnumerator sc)
+        {
+            if (!crs.ContainsKey(m))
+            {
+                crs.Add(m, new List<CoroutineInstance>());
+                CoroutineInstance ci = new CoroutineInstance();
+                ci.starter = sc;
+                ci.running = CustomStart(m, sc);
+                crs[m].Add(ci);
+            }
+            else
+            {
+                CoroutineInstance ci = crs[m].Find(c => c.starter == sc);
+                if (ci != null)
+                {
+
+                }
+                else
+                {
+                    ci = new CoroutineInstance();
+                    ci.starter = sc;
+                    ci.running = CustomStart(m, sc);
+                    crs[m].Add(ci);
+                }
+            }
+        }
     }
 }
